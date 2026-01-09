1ef4a1aef1f763a445bbf0a070069a4547e746a2
heapy <63428380+heapyxyz@users.noreply.github.com>
2026-01-09 14:20:48 +0100

    feat: improve console (like in gc)

---
+++ Launcher/Helpers/Terminal.cs
@@ -0,0 +1,52 @@
+﻿using Spectre.Console;
+
+namespace Launcher.Helpers;
+
+public static class Terminal
+{
+    private const string PrimaryColor = "DeepPink1_1";
+    private const string SecondaryColor = "gray";
+    private const string InfoColor = "gray";
+    private const string SuccessColor = "green";
+    private const string WarningColor = "darkorange";
+    private const string ErrorColor = "red";
+    private const string DebugColor = "deepskyblue1";
+
+    private const string Prefix = $"[{SecondaryColor}][[[/]" + $"[{PrimaryColor}]Launcher[/]" +
+                                  $"[{SecondaryColor}]]][/]";
+
+    public static void PrintWelcome()
+    {
+        AnsiConsole.MarkupLine(
+            $"[{PrimaryColor}]Launcher[/] [black on {PrimaryColor}] ALPHA [/] [{SecondaryColor}]made by[/] [black on {PrimaryColor}] heapy [/]");
+    }
+
+    public static void Print(object message)
+    {
+        AnsiConsole.MarkupLine($"{Prefix} [white on {InfoColor}] ? [/] {Markup.Escape(message.ToString())}");
+    }
+
+    public static void Success(object message)
+    {
+        AnsiConsole.MarkupLine(
+            $"{Prefix} [white on {SuccessColor}] ✓ [/] [{SuccessColor}]{Markup.Escape(message.ToString())}[/]");
+    }
+
+    public static void Warning(object message)
+    {
+        AnsiConsole.MarkupLine(
+            $"{Prefix} [white on {WarningColor}] ! [/] [{WarningColor}]{Markup.Escape(message.ToString())}[/]");
+    }
+
+    public static void Error(object message)
+    {
+        AnsiConsole.MarkupLine(
+            $"{Prefix} [white on {ErrorColor}] X [/] [{ErrorColor}]{Markup.Escape(message.ToString())}[/]");
+    }
+
+    public static void Debug(object message)
+    {
+        AnsiConsole.MarkupLine(
+            $"{Prefix} [white on {DebugColor}] ? [/] [{DebugColor}]{Markup.Escape(message.ToString())}[/]");
+    }
+}
--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using Launcher.Helpers;
+
+Terminal.PrintWelcome();
+
+if (Debugger.IsAttached)
+{
+    Terminal.Print("This is an info message");
+    Terminal.Success("This is a success message");
+    Terminal.Warning("This is a warning message");
+    Terminal.Error("This is an error message");
+    Terminal.Debug("This is a debug message");
+}
