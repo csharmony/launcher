a189c1e0b436e04dd64b4e3e19a48dc6f3d239e3
heapy <63428380+heapyxyz@users.noreply.github.com>
2026-07-08 14:18:45 +0200

    feat: error handling and logging to `Game.Launch`

--- Launcher/Helpers/Game.cs
+++ Launcher/Helpers/Game.cs
@@ -6,25 +6,34 @@ public static class Game
 {
     public static async Task Launch(string fileName)
     {
-        var arguments = string.Join(" ", Environment.GetCommandLineArgs().Skip(1));
-        if (!string.IsNullOrWhiteSpace(arguments))
-            Terminal.Print($"Arguments: {arguments}");
-
-        var startInfo = new ProcessStartInfo
+        try
         {
-            FileName = Path.Combine(Steam.GamePath, fileName),
-            Arguments = arguments
-        };
+            var arguments = string.Join(" ", Environment.GetCommandLineArgs().Skip(1));
+            if (!string.IsNullOrWhiteSpace(arguments))
+                Terminal.Print($"Arguments: {arguments}");
 
-        using Process process = new() { StartInfo = startInfo };
-        process.Start();
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = Path.Combine(Steam.GamePath, fileName),
+                Arguments = arguments
+            };
 
-        Terminal.Success($"Launched Harmony successfully!");
-        if (!string.IsNullOrWhiteSpace(arguments))
-            Terminal.Print($"Arguments: {arguments}");
+            using Process process = new() { StartInfo = startInfo };
+            process.Start();
 
-        await process.WaitForExitAsync();
-        if (Debugger.IsAttached)
-            Terminal.Debug($"Game closed with exit code: {process.ExitCode}");
+            Terminal.Success($"Launched Harmony");
+            if (!string.IsNullOrWhiteSpace(arguments))
+                Terminal.Print($"Arguments: {arguments}");
+
+            await process.WaitForExitAsync();
+            if (Debugger.IsAttached)
+                Terminal.Debug($"Game closed with exit code: {process.ExitCode}");
+        }
+        catch (Exception e)
+        {
+            Terminal.Error("An error occurred while launching Harmony");
+            if (Debugger.IsAttached)
+                Terminal.Debug(e.InnerException?.Message ?? e.Message);
+        }
     }
 }
